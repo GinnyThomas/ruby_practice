ecd7c13ec18bb324458ad125721add7884832f32
Ginny <ginnynjon@gmail.com>
2021-12-17 18:08:30 +0000

    first step of meetings test passed

---
+++ lib/meetings.rb
@@ -0,0 +1,15 @@
+names= "Fred:Corwill"
+def meeting(names)
+
+  name_array = names.upcase.split(':')
+  name_string = name_array.reverse.join(', ')
+  #name_array = name_string.to_a
+
+
+end
+
+# I want to
+# make everything capital
+# reorder each name last name first, a comma, then first name
+# reorder list by alphabetical order
+# separate names by ()
