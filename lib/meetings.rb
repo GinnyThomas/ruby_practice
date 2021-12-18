c2602cce304704c42a08bfb666b88643cc02de86
Ginny <ginnynjon@gmail.com>
2021-12-18 12:52:45 +0000

    cleaned up code & also added another person's solution

--- lib/meetings.rb
+++ lib/meetings.rb
@@ -1,16 +1,13 @@
 names_string= "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill"
 
 
-def getNames(input)
-  new_names = input.upcase.gsub(/[:]/, ' ').split(';')
-  #p name_string = new_names.join('","' )
-  #p name_string.split.reverse.join(", ")
+def getNames(s)
+  new_names = s.upcase.gsub(/[:]/, ' ').split(';')
   new_names.map { | person | person.split.reverse.join(", ") }
 end
 
 def meeting(names_string)
-  p getNames(names_string).sort.map { |person| "(#{person})" }.join
-
+  getNames(names_string).sort.map { |person| "(#{person})" }.join
 end
 
 # I want to
@@ -19,4 +16,23 @@ end
 # reorder list by alphabetical order
 # separate names by ()
 
+
+def meeting(s)
+  new_names = s.upcase.gsub(/[:]/, ' ').split(';')
+  sorted = new_names.map { | person | person.split.reverse.join(", ") }
+  sorted.sort.map { |person| "(#{person})" }.join
+end
+
+# Other answers
 #
+#
+def meeting(s)
+  names =
+    s
+      .upcase
+      .split(";")
+      .map { |name| name.split(":") }
+      .sort_by { |name, last| [last, name] }
+      .map { |name, last| "(#{last}, #{name})" }
+      .join
+end
