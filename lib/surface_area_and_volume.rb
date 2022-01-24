25d48fde4297cc9465c1e7d4e4f1544f18da0999
Ginny <ginnynjon@gmail.com>
2022-01-24 10:20:21 +0000

    tests are passing

--- lib/surface_area_and_volume.rb
+++ lib/surface_area_and_volume.rb
@@ -1,3 +1,10 @@
 def get_size(w,h,d)
-  volume = w * h * d
+  area_and_volume_arry = []
+  area_and_volume_arry << area = 2*h*w + 2*w*d + 2*h*d
+  area_and_volume_arry << volume = w * h * d
+end
+
+cleaner
+def get_size(w,h,d)
+  [w*h*2 + w*d*2 + d*h*2, w*h*d]
 end
--- spec/surface_area_and_volume_spec.rb
+++ spec/surface_area_and_volume_spec.rb
@@ -1,7 +1,19 @@
 require_relative '../lib/surface_area_and_volume'
 
 RSpec.describe 'surface area and volume of a box' do
-  it 'returns the volume of a box' do
-    expect(get_size(1, 1, 1)).to eq(1)
+  # it 'returns the volume of a box' do
+  #   expect(get_size(4, 2, 6)).to eq(48)
+  # end
+
+  # it 'returns the area of a box' do
+  #   expect(get_size(1, 1, 1)).to eq(6)
+  # end
+  #
+  it 'returns the area and volume of a box as an array' do
+    expect(get_size(1, 1, 1)).to eq([6, 1])
+  end
+
+  it 'returns the area and volume of a box as an array' do
+    expect(get_size(4, 2, 6)).to eq([88, 48])
   end
 end
