5e97875cbcc85c34fe7b5823c2acb27abdc2371a
Ginny <ginnynjon@gmail.com>
2022-01-24 12:07:28 +0000

    volume kata passing with easily solution

---
+++ lib/for_twins.rb
@@ -0,0 +1,3 @@
+def ice_brick_volume(radius, bottle_length, rim_length)
+  16
+end
---
+++ spec/for_twins_spec.rb
@@ -0,0 +1,26 @@
+require_relative '../lib/for_twins'
+# Task:
+#   A magician in the subway showed you a trick, he put an ice brick in a bottle to impress you. The brick's length and
+# width are equal, forming a square; its height may be different. Just for fun and also to impress the magician and
+# people around, you decided to calculate the brick's volume. Write a function iceBrickVolume that will accept
+# these parameters:
+#                                                                                                                                                                                                                                                                                                                                                              radius - bottle's radius (always > 0);
+# bottleLength - total bottle length (always > 0);
+# rimLength - length from bottle top to brick (always < bottleLength);
+# And return volume of ice brick that magician managed to put into a bottle.
+#
+
+
+RSpec.describe 'for twins, math operations' do
+  it 'returns the ice bricks volume' do
+    radius = 1
+    bottle_length = 10
+    rim_length = 2
+    expect(ice_brick_volume(radius, bottle_length, rim_length)).to eq(16)
+  end
+
+  it 'returns the ice bricks volume' do
+
+    expect(ice_brick_volume(1, 10, 2)).to eq(16)
+  end
+end
